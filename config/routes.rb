44ce6d9e2a54428d08a97aaa6937f1caa3d6833e
Tobias Schmidt <t.schmidt@rubidat.de>
2016-08-18 22:22:09 +0200

    user api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-
-
   use_doorkeeper
-  devise_for :users
-  
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1 do
+      get 'user', to: 'users#show'
+    end
+  end
+
   root to: "home#index"
 
 end
