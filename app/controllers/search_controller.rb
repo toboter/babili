be49d08f806f1eddb6d8345c50b65fdc1a8886e6
Tobias Schmidt <t.schmidt@rubidat.de>
2016-11-23 14:18:27 +0100

    fixing app name treatment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,8 +7,8 @@ class SearchController < ApplicationController
       @results =[]
       @apps.each do |app|
         resp = nil
-        instance_variable_set("@#{app.name}_url", "#{app.url}?q=#{url_encode(params[:q])}")
-        uri = URI(instance_variable_get("@#{app.name}_url"))
+        instance_variable_set("@#{app.name.parameterize.underscore}_url", "#{app.url}?q=#{url_encode(params[:q])}")
+        uri = URI(instance_variable_get("@#{app.name.parameterize.underscore}_url"))
         ## hier muss noch dazwischen geschaltet werden, falls der Server nicht erreichbar ist.
         resp = Net::HTTP.get_response(uri)
         @results.concat(JSON.parse(resp.body)) if resp
