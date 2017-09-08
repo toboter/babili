59514762a6d15c63aca3ddf285ef15f9c1959dca
Tobias Schmidt <t.schmidt@rubidat.de>
2017-09-08 16:23:25 +0200

    adding attr for urls to search app serializer

--- app/serializers/oread_application_serializer.rb
+++ app/serializers/oread_application_serializer.rb
@@ -1,17 +1,26 @@
 class OreadApplicationSerializer < ActiveModel::Serializer
   include ActiveModel::Serializers
+  include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :owner, :user_access_token, :url, :scope
-
-  def owner
-    object.owner.try(:id)
-  end
+  attributes :id, :name, :host, :port, :path, :url, :user_access_token, :provider_presentation_human
 
   def user_access_token
     object.access_tokens.where(resource_owner: Thread.current[:current_user]).last.try(:token)
   end
 
-  def scope
-    'all'
+  def path
+    object.search_path
   end
+
+  def provider_presentation_human
+    url_for([object, {only_path: Rails.env.test?}])
+  end
+  
+  attribute :owner do
+    {
+      name: object.owner.try(:name),
+      profile_human: url_for([object.owner.profile, {only_path: Rails.env.test?}])
+    }
+  end
+
 end
