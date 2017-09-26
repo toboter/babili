cbb2da81ebc3cf00247f741069004278b7f1596b
Tobias Schmidt <t.schmidt@rubidat.de>
2017-09-26 12:13:23 +0200

    adding image_thumb_50 to user api

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,8 +5,10 @@ class UserSerializer < ActiveModel::Serializer
     object.name
   end
   
-  def image
-    object.profile.image(:small_thumb) if object.profile
+  def image_thumb_50
+    "http://#{Rails.application.routes.default_url_options[:host]}
+    #{':'+Rails.application.routes.default_url_options[:port].to_s if Rails.application.routes.default_url_options[:port]}
+    #{object.profile.image_url(:small_thumb)}".squish.gsub(/\s+/, "") if object.profile
   end
 
 end
