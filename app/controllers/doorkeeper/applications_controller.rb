96b7916e671bf7aef694859f710347364af3acc6
Tobias Schmidt <t.schmidt@rubidat.de>
2017-09-29 23:23:41 +0200

    fixing doorkeeper update bug

--- app/controllers/doorkeeper/applications_controller.rb
+++ app/controllers/doorkeeper/applications_controller.rb
@@ -2,5 +2,48 @@ module Doorkeeper
   class ApplicationsController < ActionController::Base
     before_action :authenticate_user!, except: [:index, :show]
     load_and_authorize_resource :application, class: 'Doorkeeper::Application'
+    
+    def index
+      @applications = Application.all
+    end
+
+    def show; end
+
+    def new
+      @application = Application.new
+    end
+    
+    def create
+      @application = Application.new(application_params)
+      if @application.save
+        flash[:notice] = I18n.t(:notice, scope: [:doorkeeper, :flash, :applications, :create])
+        redirect_to oauth_application_url(@application)
+      else
+        render :new
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @application.update_attributes(application_params)
+        flash[:notice] = I18n.t(:notice, scope: [:doorkeeper, :flash, :applications, :update])
+        redirect_to oauth_application_url(@application)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      flash[:notice] = I18n.t(:notice, scope: [:doorkeeper, :flash, :applications, :destroy]) if @application.destroy
+      redirect_to oauth_applications_url
+    end
+
+    private
+
+    def application_params
+      params.require(:doorkeeper_application).permit(:name, :redirect_uri, :scopes)
+    end
+    
   end
 end
