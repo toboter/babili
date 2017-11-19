f477151013de45415e112cf763d5d1113dea9f36
Tobias Schmidt <t.schmidt@rubidat.de>
2017-11-19 16:48:43 +0100

    correcting class name from base to content

--- db/migrate/20171117224829_rename_type_content_in_cms_contents.rb
+++ db/migrate/20171117224829_rename_type_content_in_cms_contents.rb
@@ -1,15 +1,15 @@
 class RenameTypeContentInCMSContents < ActiveRecord::Migration[5.0]
   def up
-    CMS::Base.inheritance_column = false
-    CMS::Base.where(type: 'About').update_all(type: 'CMS::BlogPage', type_details: {featured: true})
-    CMS::Base.where(type: 'Post').update_all(type: 'CMS::BlogPage')
-    CMS::Base.where(type: 'Novelity').update_all(type: 'CMS::Novelity')
+    CMS::Content.inheritance_column = false
+    CMS::Content.where(type: 'About').update_all(type: 'CMS::BlogPage', type_details: {featured: true})
+    CMS::Content.where(type: 'Post').update_all(type: 'CMS::BlogPage')
+    CMS::Content.where(type: 'Novelity').update_all(type: 'CMS::Novelity')
   end
 
   def down
-    CMS::Base.inheritance_column = false
-    CMS::Base.where(type: 'CMS::Blog').where('type_details @> ?', {featured: true}.to_json).update_all(type: 'About')
-    CMS::Base.where(type: 'CMS::Blog').update_all(type: 'Post')
-    CMS::Base.where(type: 'CMS::Novelity').update_all(type: 'Novelity')
+    CMS::Content.inheritance_column = false
+    CMS::Content.where(type: 'CMS::Blog').where('type_details @> ?', {featured: true}.to_json).update_all(type: 'About')
+    CMS::Content.where(type: 'CMS::Blog').update_all(type: 'Post')
+    CMS::Content.where(type: 'CMS::Novelity').update_all(type: 'Novelity')
   end
 end
