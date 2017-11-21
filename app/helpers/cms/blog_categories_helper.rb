2ba2106ced6b17bef75969bd16902dd2f33637f9
Tobias Schmidt <t.schmidt@rubidat.de>
2017-11-21 16:46:33 +0100

    visualize HelpPage as tree

--- app/helpers/cms/blog_categories_helper.rb
+++ app/helpers/cms/blog_categories_helper.rb
@@ -1,2 +1,18 @@
 module CMS::BlogCategoriesHelper
+
+  def view_page_tree_for(pages)
+    html = content_tag(:ul, class: 'category-articles chevron') {
+      ul_contents = ""
+      pages.each do |page|
+        if page.present?
+          ul_contents << content_tag(:li, link_to(page.title, page, style: "margin-left:#{page.depth*10}px"), class: "help-link", id: "help-article-#{page.id}")
+          if page.children.any?
+            ul_contents << view_page_tree_for(page.children)
+          end
+        end
+      end
+      ul_contents.html_safe
+    }.html_safe
+  end
+
 end
