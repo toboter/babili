bbe8efe4ece77e36127a04cc173f4e273d2997ab
Tobias Schmidt <t.schmidt@rubidat.de>
2017-12-14 01:48:58 +0100

    fixing repo urls in serializers

--- app/serializers/oread_application_serializer.rb
+++ app/serializers/oread_application_serializer.rb
@@ -2,14 +2,11 @@ class OreadApplicationSerializer < ActiveModel::Serializer
   include ActiveModel::Serializers
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :host, :port, :path, :url, :user_access_token, :provider_presentation_human
+  attributes :id, :name, :host, :port, :url, :user_access_token, :provider_presentation_human
+  has_many :repository_classes
 
   def user_access_token
     object.access_tokens.where(resource_owner: current_user).last.try(:token)
-  end
-
-  def path
-    object.search_path
   end
 
   def provider_presentation_human
