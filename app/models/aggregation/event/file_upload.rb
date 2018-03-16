6f6af6d07c06cf763b3e19c23efa22b85a6bb86a
Tobias Schmidt <t.schmidt@rubidat.de>
2018-03-16 13:00:24 +0100

    bugfix

--- app/models/aggregation/event/file_upload.rb
+++ app/models/aggregation/event/file_upload.rb
@@ -1,6 +1,5 @@
 class Aggregation::Event::FileUpload < Aggregation::Event
   include FileUploader[:file]
-  include Processable
   require 'roo'
 
   SCHEMA_TYPES = %w(Lido Tabular)
@@ -23,6 +22,15 @@ class Aggregation::Event::FileUpload < Aggregation::Event
       when "xml" then self.parse_xml_file #Aggregation::Commit::ProcessXmlJob.perform_later(self.id)
       else raise "Unknown file type: #{file.data['metadata']['filename']}"
     end
+  end
+
+  def parse_xml_file
+    doc = Nokogiri::XML(File.open(self.file.download.path))
+    json = JSON.parse(JSON.generate(doc.root))
+    item = Aggregation::Item.joins(:identifiers)
+        .where(identifiers: { origin_id: event.processor.primary_id_label } )
+        .first_or_create(repository_id: event.repository_id)
+    Aggregation::Commit.create(item_id: item, event_id: self, creator_id: self.creator_id, data: json)
   end
 
   def open_spreadsheet
