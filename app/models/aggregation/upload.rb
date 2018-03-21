f905abc1e12099d6c3857e6ece19a645ae45b364
Tobias Schmidt <t.schmidt@rubidat.de>
2018-03-21 19:06:01 +0100

    bugfix

--- app/models/aggregation/upload.rb
+++ app/models/aggregation/upload.rb
@@ -1,6 +1,6 @@
 # uploaded files
 class Aggregation::Upload < ApplicationRecord
-  include FileUploader[:file]
+  include Aggregation::FileUploader[:file]
   belongs_to :upload_event, class_name: 'Aggregation::Event::UploadEvent', foreign_key: :event_id
 
 end
