5a50671ec435af46be80f01369583b0c6229eba8
Tobias Schmidt <t.schmidt@rubidat.de>
2018-05-07 16:29:53 +0200

    test

--- app/models/aggregation/commit/lido.rb
+++ app/models/aggregation/commit/lido.rb
@@ -3,4 +3,6 @@ class Aggregation::Commit::Lido < Aggregation::Commit
   include JsonAttribute::Record::QueryScopes
   store_accessor :data, :identifier, :changeset
   json_attribute :payload, Aggregation::Commit::Lido::Base.to_type, container_attribute: "data"
+
+  # was muss hier passieren?
 end
