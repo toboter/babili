84b2d2178b0e5705fbf223080f0c184279bc8a27
Tobias Schmidt <t.schmidt@rubidat.de>
2018-05-16 12:05:15 +0200

    adding cors, using hashes on concept index

--- app/models/aggregation/identifier.rb
+++ app/models/aggregation/identifier.rb
@@ -6,7 +6,11 @@
 class Aggregation::Identifier < ApplicationRecord
   has_and_belongs_to_many :items, join_table: "aggregation_identifications", foreign_key: :identifier_id
   has_many :items_with_pref_identifiers, class_name: 'Aggregation::Item', foreign_key: :pref_identifier_id
+  has_many :identifiers, through: :items
 
   validates :origin_id, uniqueness: { scope: :origin_type,
     message: "only one identifier" }
 end
+
+# Aggregation::Identifier.first.identifiers
+# Aggregation::Identifier.first.identifiers.map{|i| i.items_with_pref_identifiers}
