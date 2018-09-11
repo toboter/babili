cd8e8240faf5ab88d31e3ebc2f3fdb653dfe7032
Tobias Schmidt <t.schmidt@rubidat.de>
2018-09-11 18:01:17 +0200

    adding new collabs

--- app/controllers/repo/settings/collaborations_controller.rb
+++ app/controllers/repo/settings/collaborations_controller.rb
@@ -7,7 +7,7 @@ class Repo::Settings::CollaborationsController < ApplicationController
   def index
     authorize! :read_collaborations, @repository
     @collaborations = @repository.collaborations
-    @potential_collabs = Person.all
+    @potential_collabs = Person.all - @repository.accessors - @repository.invited_collaborators
   end
 
   def new
@@ -31,7 +31,7 @@ class Repo::Settings::CollaborationsController < ApplicationController
 
   def update
     respond_to do |format|
-      if @collaboration.update(repository_collaboration_params)
+      if @collaboration.update(resource_params)
         format.html { redirect_to namespace_repository_settings_collaborations_path(@namespace, @repository), notice: "Collaboration was successfully updated." }
         format.json { render :show, status: :ok, location: @collaboration }
         format.js { render js: "toastr['success']('Successfully updated collaboration!');", status: 200 }
@@ -44,10 +44,15 @@ class Repo::Settings::CollaborationsController < ApplicationController
   end
 
   def destroy
+    @collaboration.destroy
+    respond_to do |format|
+      format.html { redirect_to namespace_repository_settings_collaborations_path(@namespace, @repository), notice: 'Collaborator was successfully removed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-  def repository_collaboration_params
+  def resource_params
     params.require(:repository_collaboration).permit(
       :collaborator_id,
       :can_manage,
